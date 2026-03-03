43591e91b8044af1ababde56fcb3990fa52577dc
oylonq <oylonq@gmail.com>
2026-03-03 18:30:15 +0800

    Ex1

---
+++ Ex1-1/Ex1-1.cpp
@@ -0,0 +1,11 @@
+// Exercise 1.1: Review the documentation for your compiler
+// and determine what file naming convention it uses. Compile
+// and run the main program from page 2.
+
+// Answer: C++ source files conventionally use one of the suffixes .C, .cc,
+// .cpp, .CPP, .c++, .cp, or .cxx; C++ header files often use .hh, .hpp, .H, or
+// (for shared template code) .tcc; preprocessed C++ files use the suffix .ii;
+// and C++20 module interface units sometimes use .ixx, .cppm, .cxxm, .c++m, or
+// .ccm.
+
+int main() { return 0; }
